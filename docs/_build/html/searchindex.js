2115ba8a7c9a15d2bd184fe9ee7563f8330f7a30
elenacuoco <carpediem08>
2017-02-21 11:05:00 +0100

    modified docs

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:51,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to p4TSA and pyWDFml documentation"],titleterms:{document:0,indic:0,p4tsa:0,pywdfml:0,tabl:0,welcom:0}})
