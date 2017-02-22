7a46057e1aa248a2b158af207be11d4afa0df101
elenacuoco <elena.cuoco@gmail.com>
2017-02-22 20:53:18 +0100

    modified travis and documentations

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode']
-
+extensions = [ "breathe" ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
