a42c66ce7ce665cd9790ad1ad8e02ef9b11b86d6
elenacuoco <elena.cuoco@gmail.com>
2017-02-22 23:40:07 +0100

    modified install_dependencies.sh

--- python-wrapper/setup.py
+++ python-wrapper/setup.py
@@ -8,8 +8,8 @@ import os
 __version__ = '0.0.1'
 
 ENV_INCLUDE=os.environ['ENV_ROOT']+'/include'
-os.environ["CC"] = "gcc-5.3"
-os.environ["CXX"] = "g++-5.3"
+os.environ["CC"] = "gcc-5"
+os.environ["CXX"] = "g++-5"
 os.environ["CLANG"] = "clang-3.8"
 ext_modules = [
     Extension(
@@ -31,7 +31,7 @@ setup(
     long_description='',
     ext_modules=ext_modules,
     install_requires=['pybind11>=1.7'],
-    extra_compile_args=['-std=c++0'],
+    extra_compile_args=['-std=c++11'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     zip_safe=False,
