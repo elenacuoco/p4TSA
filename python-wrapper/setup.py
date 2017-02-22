b065e5f7b9fad8b2ad87e8f025e25525f638da67
elenacuoco <elena.cuoco@gmail.com>
2017-02-22 23:55:22 +0100

    modified install_dependencies.sh

--- python-wrapper/setup.py
+++ python-wrapper/setup.py
@@ -6,8 +6,9 @@ import os
 
 
 __version__ = '0.0.1'
-ENV_ROOT="${TRAVIS_BUILD_DIR}/NewEnv"
-ENV_INCLUDE=os.environ['ENV_ROOT']+'/include'
+#ENV_ROOT="${TRAVIS_BUILD_DIR}/NewEnv"
+#ENV_INCLUDE=os.environ['ENV_ROOT']+'/include'
+ENV_INCLUDE="${TRAVIS_BUILD_DIR}/NewEnv/include"
 
 ext_modules = [
     Extension(
