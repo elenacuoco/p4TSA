335cf58931fcceaf6a4ff99dd5a016ddda72a577
elenacuoco <carpediem08>
2017-02-22 17:41:55 +0100

    modified travis and documentations

--- python-wrapper/setup.py
+++ python-wrapper/setup.py
@@ -7,29 +7,13 @@ import os
 __version__ = '0.0.1'
 
 ENV_INCLUDE=os.environ['ENV_ROOT']+'/include'
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path
-
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
-
-    def __init__(self, user=False):
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-        return pybind11.get_include(self.user)
-
 
 ext_modules = [
     Extension(
         'pytsa',
         ['pytsa.cpp'],
         include_dirs=['../include',ENV_INCLUDE,
-            # Path to pybind11 headers
-            get_pybind_include(),
-            get_pybind_include(user=True)
+
         ],
         libraries=[ 'gsl', 'Frame', 'fftw3','fftw3f', 'fftw3l', 'gslcblas','p4TSA'],
         language='c++'
