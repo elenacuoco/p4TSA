d0dcaa37b235ebeff36e7b795d543545e86be428
elenacuoco <carpediem08>
2017-02-22 18:01:15 +0100

    modified travis and documentations

--- docs/conf.py
+++ docs/conf.py
@@ -171,4 +171,4 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 
-    subprocess.call('doxygen ../Doxyfile', shell=True)
+    subprocess.call('doxygen ./Doxyfile', shell=True)
