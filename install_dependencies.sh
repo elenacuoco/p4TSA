dd05fc3d521b0409c649fa2ab6f3887c5530277f
elenacuoco <carpediem08>
2017-02-22 13:28:06 +0100

    modified travis and documentations

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -31,8 +31,27 @@ cp -fr ./include/pybind11/ ${ENV_ROOT}/include/python$PYTHON_VERSION
 popd 
 popd 
 
+# ----------------------------------------------------------
+#
+# cmake installation
+#
 
+pushd $ENV_TMP
+wget https://cmake.org/files/v3.8/cmake-3.8.0-rc1.tar.gz
+tar xvfz cmake-3.8.0-rc1.tar.gz
+pushd cmake-3.8.0-rc1
+./configure --prefix=${ENV_ROOT}
+make -j8 install
+popd
+popd
 
+echo export ENV_ROOT=$ENV_ROOT > $ENV_ROOT/environment
+echo export CMAKE_ROOT=${ENV_ROOT}/share/cmake-3.8 >> $ENV_ROOT/environment
+echo export PYTHONPATH=${ENV_ROOT}/lib/python$PYTHON_VERSION:${ENV_ROOT}/lib/python$PYTHON_VERSION/site-packages >> $ENV_ROOT/environment
+
+cat environment >> $ENV_ROOT/environment
+
+source ${ENV_ROOT}/environment
 # ----------------------------------------------------------
 #
 # Installing basic environment
