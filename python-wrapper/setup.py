e5d00ada77d92e58b866cdaa30d080ff604ffab5
elenacuoco <elena.cuoco@gmail.com>
2017-02-22 23:45:44 +0100

    modified install_dependencies.sh

--- python-wrapper/setup.py
+++ python-wrapper/setup.py
@@ -8,9 +8,7 @@ import os
 __version__ = '0.0.1'
 
 ENV_INCLUDE=os.environ['ENV_ROOT']+'/include'
-os.environ["CC"] = "gcc-5"
-os.environ["CXX"] = "g++-5"
-os.environ["CLANG"] = "clang-3.8"
+
 ext_modules = [
     Extension(
         'pytsa',
