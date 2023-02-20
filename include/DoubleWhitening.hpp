456b6286515a116b8cc4187ba25d2cc2657e2d62
Elena Cuoco <elena.cuoco@gmail.com>
2023-02-20 12:48:50 +0100

    Update DoubleWhitening.hpp

--- include/DoubleWhitening.hpp
+++ include/DoubleWhitening.hpp
@@ -313,12 +313,8 @@ namespace tsa {
         ///
         //@{
         void SetData(Dmatrix &Data, double scale);
-        void SetOutputSize(unsigned int OutputSize,unsigned int ExtraSize){
-            mOutputSize=OutputSize;
-            mTotSize=OutputSize+ExtraSize;
-            mWhitened(1, mTotSize);
-      
-        }
+        void SetOutputSize(unsigned int OutputSize,unsigned int ExtraSize);
+    
       
         //@}
 
