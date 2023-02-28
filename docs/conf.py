daed6ba7bf6cf635f224710bf4ab986c80fd8a5e
Elena Cuoco <elena.cuoco@gmail.com>
2023-02-28 15:14:22 +0100

    modified doc

--- docs/conf.py
+++ docs/conf.py
@@ -17,20 +17,19 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
+ 
 import sys
 sys.path.insert(0, os.path.abspath('../python-wrapper'))
 sys.path.insert(0, os.path.abspath('../src'))
 sys.path.insert(0, os.path.abspath('../include'))
 
-
-import subprocess
+import subprocess, os
 
 def configureDoxyfile(input_dir, output_dir):
     with open('Doxyfile.in', 'r') as file :
         filedata = file.read()
 
-    #filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
     filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
 
     with open('Doxyfile', 'w') as file:
@@ -38,15 +37,23 @@ def configureDoxyfile(input_dir, output_dir):
 
 # Check if we're running on Read the Docs' servers
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+# -- Breathe configuration -------------------------------------------------
 
-breathe_projects = {}
-
+breathe_projects = {"package for Time Serie Analysis:p4TSA": "_build/xml/"}
+breathe_default_project = "C++ Sphinx Doxygen Breathe"
+breathe_default_members = ('members', 'undoc-members')
 if read_the_docs_build:
-    input_dir = '../src'
+    input_dir = '../src ../include ../python-wrapper'
     output_dir = 'build'
     configureDoxyfile(input_dir, output_dir)
     subprocess.call('doxygen', shell=True)
     breathe_projects['p4TSA'] = output_dir + '/xml'
+
+# ...
+
+
+
+
 
 
 # -- General configuration ------------------------------------------------
@@ -58,20 +65,24 @@ if read_the_docs_build:
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.autosectionlabel',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'breathe']
-
+    'sphinx_sitemap',
+    'sphinx.ext.inheritance_diagram',
+    'breathe'
+]
 breathe_default_project = "p4TSA"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+highlight_language = 'c++'
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -120,13 +131,31 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 # html_theme_options = {}
+
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    
+    'logo_only': False,
+
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+# html_logo = ''
+github_url = 'https://github.com/elenacuoco/p4TSA'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -189,7 +218,6 @@ texinfo_documents = [
      author, 'p4TSA', 'One line description of project.',
      'Miscellaneous'),
 ]
-
 
 
 
